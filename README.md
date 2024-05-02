aeb2a6d9d2a1dac2aca6395a401740800cea6970
Reuben Lewis <reuben.lewis@outlook.com>
2024-05-01 21:49:53 -0600

    refactor: setup for more commands

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Speaker Box Dimension Finder
 
+Calculates the dimensions of a speaker based on a specified volume in cm^3.
+
+Currently supports using the golden ratio and the square root of 2.
+
 ```shell
-❯ python dimension_finder.py calculate-golden-ratio-box --help
-Usage: dimension_finder.py calculate-golden-ratio-box [OPTIONS] BOX_VOLUME_CM3
+❯ python dimension_finder.py --help
+Usage: dimension_finder.py [OPTIONS] COMMAND [ARGS]...
 
 Options:
   -s, --start-cm FLOAT
@@ -10,5 +14,16 @@ Options:
   -x, --step-size FLOAT
   -r, --exit-range FLOAT
   --help                  Show this message and exit.
+
+Commands:
+  calculate-golden-ratio
 ```
+
+Example:
+```shell
+python dimension_finder.py -r 250 -x 0.1  calculate-golden-ratio 25000
+```
+
+Starts the search at 20cm for the height, and steps by 0.1 cm until the calculated volume is within +/- 250 cm^3 of the specified volume, 25,000.
+
 
--- dimension_finder.py
+++ dimension_finder.py
@@ -9,26 +9,35 @@ GOLDEN_RATIO_NARROW = 1.618 - 1
 
 
 @click.group()
-def cli():
-    pass
-
-
-@cli.command()
+@click.pass_context
 @click.option("--start-cm", "-s", type=float, default=20)
 @click.option("--end-cm", "-e", type=float, default=40)
 @click.option("--step-size", "-x", type=float, default=0.5)
 @click.option("--exit-range", "-r", type=float, default=100)
-@click.argument("box_volume_cm3", type=float)
-def calculate_golden_ratio_box(
-    box_volume_cm3: float, start_cm: float, end_cm: float, step_size: float, exit_range: float
+def cli(
+    ctx, start_cm: float, end_cm: float, step_size: float, exit_range: float
 ):
+    ctx.obj = {}
+    ctx.obj["start"] = start_cm
+    ctx.obj["end"] = end_cm
+    ctx.obj["step"] = step_size
+    ctx.obj["exit_range"] = exit_range
+
+
+@cli.command()
+@click.argument("box_volume_cm3", type=float)
+@click.pass_context
+def calculate_golden_ratio(ctx, box_volume_cm3: float):
     logger.info("*** Finding parameters for box with volume %s cm^3 ***", box_volume_cm3)
     calc_volume_cm3 = 0
-    height_cm = start_cm
+    height_cm = ctx.obj["start"]
     width_cm = 0
     depth_cm = 0
 
-    while height_cm < end_cm:
+    upper_bound = box_volume_cm3 + ctx.obj["exit_range"]
+    lower_bound = box_volume_cm3 - ctx.obj["exit_range"]
+
+    while height_cm < ctx.obj["end"]:
         width_cm = height_cm * GOLDEN_RATIO
         depth_cm = height_cm * GOLDEN_RATIO_NARROW
         calc_volume_cm3 = height_cm * width_cm * depth_cm
@@ -41,8 +50,8 @@ def calculate_golden_ratio_box(
         )
 
         if (
-            calc_volume_cm3 <= box_volume_cm3 + exit_range
-            and calc_volume_cm3 >= box_volume_cm3 - exit_range
+            calc_volume_cm3 <= upper_bound
+            and calc_volume_cm3 >= lower_bound
         ):
             logger.info(
                 "*** Found size: %s x %s x %s = %s ***",
@@ -53,7 +62,7 @@ def calculate_golden_ratio_box(
             )
             break
 
-        height_cm += step_size
+        height_cm += ctx.obj["step"]
 
 if __name__ == "__main__":
     cli()
